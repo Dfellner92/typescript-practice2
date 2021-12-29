aab94196f80229648e96f4f1a19599e86b62ec71
Dan Fellner <dfellner92@gmail.com>
2021-12-28 23:24:06 -0500

    set up

--- hello-world.js
+++
@@ -1,9 +0,0 @@
-function log(str) {
-    console.log(str);
-}
-const greeting = 'hello world';
-log(greeting);
-
-
-// tsc --target es6 hello-world.ts
-//       will compile to js
---
+++ src/index.js
@@ -0,0 +1,6 @@
+function multiply(a, b) {
+    return a * b;
+}
+var a = 5;
+var b = 4;
+console.log("".concat(a, " * ").concat(b, " = ").concat(multiply(a, b)));
